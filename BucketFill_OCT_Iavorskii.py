ef6dd159a60155b38989edeea2225bc04c7a3587
Alexander <yavalvas@gmail.com>
2016-02-12 12:27:32 +0100

    Update BucketFill_OCT_Iavorskii.py
    Delete task description

--- BucketFill_OCT_Iavorskii.py
+++ BucketFill_OCT_Iavorskii.py
@@ -1,22 +1,3 @@
-"""
-Bucket Fill Exercise
-
-Imagine you are working on an image editing application. You need to implement a bucket fill tool similar to the one
-in paint. The user will use the tool by selecting a color and clicking on the canvas. The tool fills the selected
-region of color with the new color.
-
-When a pixel is filled, all of its neighbors (above, below, left, or right) of the same color must also be filled,
-as well as their neighbors, and so on, until the entire region has been filled with the new color.
-
-In this exercise, you must write *TWO* implementations of the tool. Each implementation must be different. It is not
-required that you invent the solutions yourself. You are encouraged to research the problem. Please write documentation
-explaining the difference of each implementation, such as when one solution might be more appropriate than the other.
-Don't forget to validate input. There is one existing test, however, you might consider adding some more. Keep in mind
-that although the given canvas is small, the solution should be applicable for a real canvas that could have huge
-resolutions.
-
-Please use python3 to complete this assignment.
-"""
 import numpy
 from scipy.ndimage import label
 import matplotlib.pyplot as plt
